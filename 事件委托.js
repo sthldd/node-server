643635b439c3b5c724839899507fb6fe74ebbddf
mlx <1209624528@qq.com>
2019-02-22 23:34:19 +0800

    add js

---
+++ 事件委托.js
@@ -0,0 +1,17 @@
+function listen(element, eventType, selector, fn) {
+  element.addEventListener(eventType, e => {
+    let el = e.target
+    while (!el.matches(selector)) {
+      if (element === el) {
+        el = null
+        break
+      }
+      el = el.parentNode
+    }
+    el && fn.call(el, e, el)
+  })
+  return element
+}
+listen(ul, 'click', 'li', function () {
+
+})
